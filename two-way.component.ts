aa2ac387ef6951c29dc887f57843f3046cf486c7
tvnrmurali <34337248+tvnrmurali@users.noreply.github.com>
2019-01-10 17:58:11 +0530

    Add files via upload

---
+++ two-way.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    
+    selector: 'two',
+    templateUrl: 'two-way.component.html',
+    
+})
+export class TwoWayComponent {
+
+    fName:string='admin'
+    lName:string='cg'
+}
